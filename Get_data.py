e951c62714394ec0cb9c2d8309c96d543ab22a9f
Rye <huanghauyun_at@163.com>
2023-02-28 19:58:54 +0800

    Change some comment and structure. Add a method to get single index data.

--- Get_data.py
+++ Get_data.py
@@ -5,20 +5,34 @@ import pandas as pd
 # use efinance to get index data
 
 class Get_data:
-    def __init__(self, code_list, path = r'./'):
-        self.code_list = code_list
+    def __init__(self, path = r'./'):
         self.path = path
 
-    def get_stock_data_and_store(self):
+    def get_stock_data_and_store(self, code_list):
         '''
         get stocks in the code list and store with English columns name in self.path one by one
+
+        :params code_list: the list of trading codes of stocks
         '''
 
         # get all the data of the main contracts
-        data_dict = ef.stock.get_quote_history(self.code_list)
+        data_dict = ef.stock.get_quote_history(code_list)
         for key, df in data_dict.items():
             df.drop(labels = ['涨跌额'], inplace = True, axis = 1)
             df.columns = ["chi_name", 'code', 'date', 'open', 'close', 'high', 'low', 'volume', 'amount', 'amplitude',
                           'change_percentage', 'turnover']
 
             df.to_csv(self.path + key + ".csv", encoding = 'gbk')
+
+    def get_index_data(self, index_code):
+        '''
+
+        :param index_code: the code of the index
+        '''
+        df = ef.stock.get_quote_history(index_code)
+        df.drop(labels = ['涨跌额'], inplace = True, axis = 1)
+        df.columns = ["chi_name", 'code', 'date', 'open', 'close', 'high', 'low', 'volume', 'amount', 'amplitude',
+                      'change_percentage', 'turnover']
+
+        return df
+
