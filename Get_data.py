9fbe52b48883505a3e4ff4a942bb64002390af50
Rye <huanghauyun_at@163.com>
2023-03-05 08:38:33 +0800

    Add the parameters begin_date and end_date to the Get_data class.

--- Get_data.py
+++ Get_data.py
@@ -2,11 +2,21 @@ import efinance as ef
 import pandas as pd
 
 
-# use efinance to get index data
+# use efinance to get data
 
 class Get_data:
-    def __init__(self, path = r'./'):
+
+    def __init__(self, path = r'./', begin_date = '19000101', end_date = '20500101'):
+        '''
+
+        :param begin_date: str, the starting date of the data. eg:'19000101' represents 1900-01-01
+        :param end_date: str, the ending date of the data
+        :param path: path to store the data
+        '''
+
         self.path = path
+        self.begin_date = begin_date
+        self.end_date = end_date
 
     def get_stock_data_and_store(self, code_list):
         '''
@@ -16,7 +26,7 @@ class Get_data:
         '''
 
         # get all the data of the main contracts
-        data_dict = ef.stock.get_quote_history(code_list)
+        data_dict = ef.stock.get_quote_history(code_list, beg = self.begin_date, end = self.end_date)
         for key, df in data_dict.items():
             df.drop(labels = ['涨跌额'], inplace = True, axis = 1)
             df.columns = ["chi_name", 'code', 'date', 'open', 'close', 'high', 'low', 'volume', 'amount', 'amplitude',
@@ -29,10 +39,10 @@ class Get_data:
 
         :param index_code: the code of the index
         '''
-        df = ef.stock.get_quote_history(index_code)
+
+        df = ef.stock.get_quote_history(index_code, beg = self.begin_date, end = self.end_date)
         df.drop(labels = ['涨跌额'], inplace = True, axis = 1)
         df.columns = ["chi_name", 'code', 'date', 'open', 'close', 'high', 'low', 'volume', 'amount', 'amplitude',
                       'change_percentage', 'turnover']
 
         return df
-
